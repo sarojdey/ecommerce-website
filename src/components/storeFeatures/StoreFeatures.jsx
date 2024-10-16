98fdf3e762ba83abf0904bbc7261f690f655cbc2
SRJ <sarojkumardey.contact@gmail.com>
2024-10-16 15:17:44 +0530

    added store features bar

---
+++ src/components/storeFeatures/StoreFeatures.jsx
@@ -0,0 +1,50 @@
+import { FaTruck, FaMoneyCheckAlt, FaHeadphones } from "react-icons/fa";
+import { FaMoneyCheck } from "react-icons/fa6";
+
+export default function StoreFeatures() {
+  const features = [
+    {
+      icon: <FaTruck />,
+      title: "Free Shipping",
+      description: "Free delivery for all orders",
+    },
+    {
+      icon: <FaMoneyCheckAlt />,
+      title: "Money Guarantee",
+      description: "30 days money back",
+    },
+    {
+      icon: <FaHeadphones />,
+      title: "24/7 Support",
+      description: "Friendly 24/7 support",
+    },
+    {
+      icon: <FaMoneyCheck />,
+      title: "Secure Payment",
+      description: "All cards accepted",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-6 px-4 sm:px-16 md:px-3 py-3 shadow-sm">
+      {features.map((feature, index) => (
+        <div
+          key={index}
+          className="flex items-center md:justify-center space-x-3"
+        >
+          <span className="text-xl sm:text-4xl text-[#ff4654]">
+            {feature.icon}
+          </span>
+          <div className="flex flex-col justify-center">
+            <span className="block font-semibold text-[0.7rem] sm:text-sm">
+              {feature.title}
+            </span>
+            <span className="block text-gray-600 text-[0.6rem] sm:text-xs">
+              {feature.description}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/navbar/Navbar";
+import StoreFeatures from "../components/storeFeatures/StoreFeatures";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <StoreFeatures />
     </div>
   );
 }
