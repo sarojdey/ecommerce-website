80e4ed1008da2418992936faedc8b04381bfe621
SRJ <sarojkumardey.contact@gmail.com>
2024-10-17 00:50:35 +0530

    using mockdata

--- src/components/bestSellerProducts/BestSellerProducts.jsx
+++ src/components/bestSellerProducts/BestSellerProducts.jsx
@@ -1,43 +1,7 @@
-export default function BestSellerProducts() {
-  const products = [
-    {
-      id: 1,
-      name: "Samsung Galaxy Watch 3",
-      category: "Men Fashion",
-      price: "$1,725.00",
-      image: "/headphone1.png",
-      onSale: true,
-      rating: 5,
-    },
-    {
-      id: 2,
-      name: "Apple Watch 4 2020",
-      category: "Men Fashion",
-      price: "$1,725.00",
-      image: "/headphone1.png",
-      onSale: false,
-      rating: 5,
-    },
-    {
-      id: 3,
-      name: "iPhone XS Max Pro",
-      category: "Men Fashion",
-      price: "$1,725.00",
-      image: "/headphone1.png",
-      onSale: false,
-      rating: 5,
-    },
-    {
-      id: 4,
-      name: "Beats by Dre C 3450",
-      category: "Men Fashion",
-      price: "$1,725.00",
-      image: "/headphone1.png",
-      onSale: false,
-      rating: 5,
-    },
-  ];
+import { FaStar } from "react-icons/fa";
+import data from "../../data/data.json";
 
+export default function BestSellerProducts() {
   return (
     <div className="  sm:px-4 py-4 sm:py-8 bg-slate-100">
       <h2 className="text-3xl mt-7 font-bold text-center mb-2">
@@ -47,17 +11,17 @@ export default function BestSellerProducts() {
         Check our best seller products right now
       </p>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 p-5 gap-2 sm:gap-6">
-        {products.map((product) => (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 p-5 gap-2 sm:gap-6 sm:pl-8 md:pl-10 lg:pl-14 ">
+        {data.map((item) => (
           <div
-            key={product.id}
+            key={item.id}
             className="border bg-white rounded-md p-3 sm:p-4 shadow-sm sm:w-48 md:w-44 lg:w-56"
           >
             <div className=" flex justify-center items-center">
               <div>
                 <img
-                  src={product.image}
-                  alt={product.name}
+                  src="/headphone1.png"
+                  alt={item.product}
                   className="w-full h-full object-contain rounded"
                 />
               </div>
@@ -65,14 +29,14 @@ export default function BestSellerProducts() {
 
             <div className="mt-4  flex flex-col justify-between h-52 ">
               <div>
-                <p className="text-gray-500 text-xs">{product.category}</p>
-                <h3 className="text-sm font-semibold">{product.name}</h3>
-                <p className="text-sm font-bold">{product.price}</p>
-                <div className="flex items-center mt-2">
-                  {[...Array(product.rating)].map((_, i) => (
-                    <span key={i} className="text-yellow-500 text-sm">
-                      ★
-                    </span>
+                <p className="text-gray-500 text-xs">{item.category}</p>
+                <h3 className="text-sm font-semibold mt-2">{item.product}</h3>
+                <p className="text-sm font-bold text-green-600 mt-1">
+                  <span>₹</span> {item.price}
+                </p>
+                <div className="flex items-center mt-4">
+                  {[...Array(Math.round(item.rating))].map((_, i) => (
+                    <FaStar key={i} className="text-amber-500 text-lg mr-1" />
                   ))}
                 </div>
               </div>
