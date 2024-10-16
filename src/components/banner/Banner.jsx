06fa5882f6a00feae525d14aaa56dd931663ef34
SRJ <sarojkumardey.contact@gmail.com>
2024-10-16 17:00:16 +0530

    banner temp

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,41 @@
+const Banner = () => {
+  return (
+    <div className="relative w-full min-h-96 bg-gradient-to-r from-blue-300 to-blue-900 flex items-center justify-center p-5">
+      <div className="max-w-4xl mx-auto flex flex-col-reverse md:flex-row items-center justify-between space-x-8 h-full">
+        {/* Left Section - Text */}
+        <div className="text-white max-w-lg p-5">
+          <h2 className="text-sm uppercase mb-2 tracking-widest text-gray-200">
+            SONY WH-1001XM
+          </h2>
+          <h1 className="text-3xl font-bold mb-4">
+            Best in Hi-Res and Noise Cancelling
+          </h1>
+          <p className="text-sm leading-relaxed mb-8">
+            Experience finely tuned noise-canceling performance in a comfortable
+            headphone. Long-lasting battery life plus quick charging keeps you
+            listening for up to 35 hours since start.
+          </p>
+          <div className="space-x-4">
+            <button className="bg-[#ff4654] text-white py-3 px-6 rounded-sm text-sm font-semibold shadow-sm hover:bg-[#ff3e4e] transition">
+              Buy Now for $234
+            </button>
+            <button className="bg-transparent border border-white text-white py-3 px-6 rounded-sm text-sm font-semibold hover:bg-white hover:text-blue-900 transition">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        {/* Right Section - Headphones Image */}
+        <div className="w-full  h-80 flex items-center justify-center">
+          <img
+            src="/headphone.png"
+            alt="Sony Headphones"
+            className="h-full object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../components/banner/Banner";
 import BestSellerProducts from "../components/bestSellerProducts/BestSellerProducts";
 import Navbar from "../components/navbar/Navbar";
 import StoreFeatures from "../components/storeFeatures/StoreFeatures";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Banner />
       <StoreFeatures />
       {/* <BestSellerProducts /> */}
     </div>
