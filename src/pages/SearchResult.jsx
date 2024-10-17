ffc0fcbc09e0223d03ba3d714f7ed564b3035231
SRJ <sarojkumardey.contact@gmail.com>
2024-10-17 19:11:09 +0530

    minor changes

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -2,15 +2,10 @@ import { useParams } from "react-router-dom";
 import SearchedProducts from "../components/productCards/searchedProducts";
 import filterProductsByCategory from "../utils/filterProductsByCategory";
 import data from "../data/data.json";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 
 export default function SearchResult() {
   const { categories, keyword } = useParams();
-
-  useEffect(() => {
-    console.log(categories);
-    console.log(keyword);
-  }, [categories, keyword]);
 
   const filterData = useMemo(() => {
     return filterProductsByCategory(
