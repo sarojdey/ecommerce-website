a39bdcb6b0511a31bcb857a1f6fb47a05d22710b
SRJ <sarojkumardey.contact@gmail.com>
2024-10-18 16:38:24 +0530

    minor chnages

--- src/components/empty/Empty.jsx
+++ src/components/empty/Empty.jsx
@@ -1,15 +1,15 @@
 export default function Empty({ msg }) {
   return (
-    <div className="flex justify-center items-center min-h-screen w-full">
-      <divflex className=" flex-col justify-center  items-center">
-        <div className="w-56 h-56 flex  items-center mx-auto justify-center">
+    <div className="flex justify-center items-center min-h-[80vh] w-full">
+      <divflex className="flex flex-col justify-center  items-center">
+        <div className="w-56 h-56 flex  items-center justify-center">
           <img
             className="h-full w-full object-contain"
             src="/empty.png"
             alt="404"
           />
         </div>
-        <span className="text-3xl mx-auto font-bold text-gray-900">{msg}</span>
+        <span className="text-3xl font-bold text-gray-900">{msg}</span>
       </divflex>
     </div>
   );
